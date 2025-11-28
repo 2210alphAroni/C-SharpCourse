08fa2e6f5667c1f99f713b5f57c088bf4ce46527
Nabinur Islam Roni <nabinuri80@gmail.com>
2025-11-28 11:11:59 +0600

    variables done

---
+++ Courses/variables.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace MyApplication
+{
+    class Variables
+    {
+        static void Main(String[] args)
+        {
+            // int type
+            int age= 27;
+            Console.WriteLine(age);
+            age=45;
+            Console.WriteLine(age);
+
+            // string type
+            string mynum="wonderful";
+            Console.WriteLine(mynum);
+
+            string river ="padma";
+            Console.WriteLine(river);
+
+            // char type
+            char policy='a';
+            Console.WriteLine(policy);
+
+            // bool type
+            bool value=(23>20);
+            Console.WriteLine(value);
+
+            // double type
+            double value2 =34.01;
+            Console.WriteLine(value2);
+        }
+    }
+}
