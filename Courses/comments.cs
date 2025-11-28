7fb68576aeb0b2d20bd48c015c7757cdc2201039
Nabinur Islam Roni <nabinuri80@gmail.com>
2025-11-28 10:51:39 +0600

    Initial commit

---
+++ Courses/comments.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyApplication
+{
+  class Comments
+  {
+    static void Main(string[] args)
+    {
+    //   (Two forward slashes is single line comment.)
+    Console.WriteLine("I am Genious");
+    /* I want to build a software where people
+     will serve blood for others */
+     Console.WriteLine("Blood Donations");
+    }
+  }
+}
---
+++ Courses/output.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      Console.WriteLine("I am Learning C#");
+      Console.WriteLine("It is awesome!");
+      Console.WriteLine(3 + 3);
+    //   write & writeline is same . only difference is writeline break the line & write is inline not break.
+      Console.Write("Hello World! ");
+      Console.Write("I will print on the same line.");
+    }
+  }
+}
