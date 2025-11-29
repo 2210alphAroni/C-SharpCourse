c317b32af121596bb74a0637cb6b3952588ebe58
Nabinur Islam Roni <nabinuri80@gmail.com>
2025-11-29 12:43:45 +0600

    ok

---
+++ Courses/math.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace MyApplication
+{
+  class Math_list
+  {
+    static void Main(string[] args)
+    {
+        // max math method apply
+      Console.WriteLine(Math.Max(5, 10));  
+    //   min math method apply
+    Console.WriteLine(Math.Min(5, 10));
+    // sqrt root math method apply 
+    Console.WriteLine(Math.Sqrt(64));
+    //  abs return absolute positive value method apply
+    Console.WriteLine(Math.Abs(-4.7));
+    // math round function apply 
+    Console.WriteLine(Math.Round(9.99));
+     }
+  }
+}
--- obj/Debug/net10.0/C-SharpCourse.AssemblyInfo.cs
+++ obj/Debug/net10.0/C-SharpCourse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C-SharpCourse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6247454938343ae4a3b503f0101119620d2da9a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd3ec1764db296a01b23e0e8380dc346674ce0a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("C-SharpCourse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C-SharpCourse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
